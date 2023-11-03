695000b305509ea7a74719655459ac95eb4ae143
MarwaAshraf1812 <marashraf090@gmail.com>
2023-11-03 04:33:41 +0200

    Task 7: In-order traversal

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- * binary_tree_preorder - Function that goes through a binary tree
+ * binary_tree_inorder - Function that goes through a binary tree
  *  using in-order traversal
  * @tree: Pointer to the root node of the tree to traverse
  * @func: Pointer to a function to call for each node
