708dbcc5055f5626f60540ae461674978c1476aa
MarwaAshraf1812 <marashraf090@gmail.com>
2023-11-03 03:05:55 +0200

    Task 1: Insert left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -19,6 +19,11 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	new->parent = parent;
 	new->right = NULL;
 	new->left = parent->left;
+
+	if (parent->left != NULL)
+	{
+		parent->left->parent = new;
+	}
 	parent->left = new;
 	return (new);
 }
