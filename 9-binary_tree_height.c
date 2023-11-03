6dee2aa3b37ed626764352d644cc47f4ee1a90c9
MarwaAshraf1812 <marashraf090@gmail.com>
2023-11-03 21:08:21 +0200

    Task 9: Height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,15 +9,15 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	long unsigned int leftH = 0;
 	long unsigned int rightH = 0;
-	if (tree == NULL) 
+	if (tree == NULL)
 	{
-        return 0;
-    } 
-	else 
+		return 0;
+	}
+	else
 	{
-        leftH = binary_tree_height(tree->left);
+		leftH = binary_tree_height(tree->left);
 		rightH = binary_tree_height(tree->right);
 
-        return 1 + (leftH > rightH ? leftH : rightH);
-    }
+		return 1 + (leftH > rightH ? leftH : rightH);
+	}
 }
